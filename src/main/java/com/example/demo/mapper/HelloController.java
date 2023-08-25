b57835b518455b2a8d0bd7e34b06e89d1c281ab0
YCclue <1103295228@qq.com>
2023-08-25 16:27:24 +0800

    2

---
+++ src/main/java/com/example/demo/mapper/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String aaa(@RequestParam("name")String name){
+        log.info("请求锦鲤啊aaaaa"+name);
+        return "Hello ,Spring"+name;
+    }
+}
